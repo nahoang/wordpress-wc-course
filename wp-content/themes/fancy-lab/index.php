029fabefd3fdf3938f02dcc01d840d37d560f52e
nahoang <nahoang9698@gmail.com>
2021-11-18 15:56:30 +0700

    feat: Add basic block of site

--- wp-content/themes/fancy-lab/index.php
+++ wp-content/themes/fancy-lab/index.php
@@ -6,7 +6,35 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Fancy Lab</title>
 </head>
-<body>
-  <p>This is a test paragraph</p>
+<body <?php body_class(); ?>>
+  <div id="page" class="site">
+    <header>
+      <section class="search">Search</section>
+      <section class="top-bar">
+        <div class="brand">
+          Logo
+        </div>
+        <div class="second-column">
+          <div class="account">Account</div>
+          <nav class="main-menu">Menu</nav>
+        </div>
+      </section>
+    </header>
+    <div class="content-area">
+      <main>
+        <section class="slider">Slide</section>
+        <section class="popular-products">Popular Products</section>
+        <section class="new-arrivals">New arrivals</section>
+        <section class="deal-of-the-week">Deal of the Week</section>
+        <section class="lab-blog">News</section>
+      </main>
+    </div>
+    <footer>
+      <section class="footer-widgets">Footer Widgets</section>
+      <section class="copyright">Copyright</section>
+    </footer>
+  </div>
 </body>
 </html>
+
+
