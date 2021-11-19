ec1f6cb06ad73938c9dfb05db24a8b9ac9ca474f
nahoang <nahoang9698@gmail.com>
2021-11-19 15:52:00 +0700

    feat: Displaying the wordpress post

--- wp-content/themes/fancy-lab/functions.php
+++ wp-content/themes/fancy-lab/functions.php
@@ -20,7 +20,8 @@
  function fancy_lab_config() {
    register_nav_menus(
      array(
-       'fancy_lab_main_menu'  =>  'Fancy Lab Main Menu'
+       'fancy_lab_main_menu'  =>  'Fancy Lab Main Menu',
+       'fancy_lab_footer_menu'  =>  'Fancy Lab Footer Menu'
      )
    );
  }
--- wp-content/themes/fancy-lab/index.php
+++ wp-content/themes/fancy-lab/index.php
@@ -11,11 +11,56 @@ get_header();  ?>
 
     <div class="content-area">
       <main>
-        <section class="slider">Slide</section>
-        <section class="popular-products">Popular Products</section>
-        <section class="new-arrivals">New arrivals</section>
-        <section class="deal-of-the-week">Deal of the Week</section>
-        <section class="lab-blog">News</section>
+        <section class="slider">
+          <div class="container">
+            <div class="row">
+
+            </div>
+          </div>
+        </section>
+        <section class="popular-products">
+        <div class="container">
+            <div class="row">
+              
+            </div>
+          </div>
+        </section>
+        <section class="new-arrivals">
+          <div class="container">
+            <div class="row">
+              
+            </div>
+          </div>
+        </section>
+        <section class="deal-of-the-week">
+
+          <div class="container">
+              <div class="row">
+                
+              </div>
+            </div>
+        </section>
+        <section class="lab-blog">
+
+        <div class="container">
+            <div class="row">
+              <?php 
+              if( have_posts() ):
+                while( have_posts() ): the_post(); 
+                  ?>
+                  <article>
+                    <h2><?php the_title(); ?></h2>
+                    <div><?php the_content(); ?></div>
+                  </article>
+                  <?php
+                endwhile;
+              else:
+              ?>
+                <p>Nothing to display</p>
+              <?php endif; ?>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
    
