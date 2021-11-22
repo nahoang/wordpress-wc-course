79a5825751157e207d9684d3167ab913962a163a
nahoang <nahoang9698@gmail.com>
2021-11-22 13:52:59 +0700

    feat: Implement Page template

---
+++ wp-content/themes/fancy-lab/page.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying all pages
+ * @package Fancy Lab
+ */
+
+get_header();  ?>
+
+    <div class="content-area">
+      <main>
+        <div class="container">
+            <div class="row">
+              <?php 
+              if( have_posts() ):
+                while( have_posts() ): the_post(); 
+                  ?>
+                  <article>
+                    <h2><?php the_title(); ?></h2>
+                    <div><?php the_content(); ?></div>
+                  </article>
+                  <?php
+                endwhile;
+              else:
+              ?>
+                <p>Nothing to display</p>
+              <?php endif; ?>
+            </div>
+          </div>
+
+      </main>
+    </div>
+   
+    <?php get_footer(); ?>
+
