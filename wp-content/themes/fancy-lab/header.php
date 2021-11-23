71b0435758a7ea988d62813aa7360100c1a46e74
nahoang <nahoang9698@gmail.com>
2021-11-23 15:25:24 +0700

    feat: Including the link to account page

--- wp-content/themes/fancy-lab/header.php
+++ wp-content/themes/fancy-lab/header.php
@@ -36,12 +36,30 @@
           </div>
           <div class="second-column col-9 col-md-9 col-12 col-lg-10">
             <div class="row">
+              <?php if( class_exists('Woocommerce')): ?>
               <div class="account col-12">
+                <div class="navbar-expand">
+                  <ul class="navbar-nav float-left">
+                    <?php if ( is_user_logged_in() ): ?>
+                    <li>
+                      <a href="<?php echo esc_url(get_permalink( get_option( 'woocommerce_myaccount_page_id' ) )); ?>" class="nav-link">My Account</a>
+                    </li>
+                    <li>
+                      <a href="<?php echo esc_url( wp_logout_url( ) ); ?>" class="nav-link">Logout</a>
+                    </li>
+                    <?php else: ?>
+                      <li>
+                      <a href="<?php echo esc_url(get_permalink( get_option( 'woocommerce_myaccount_page_id' ) )); ?>" class="nav-link">Login / Register</a>
+                    </li>
+                    <?php endif; ?>
+                  </ul>
+                </div>
                 <div class="cart text-right">
                   <a href="<?php echo wc_get_cart_url(); ?>"><span class="cart-icon"></span></a>
                   <span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
                 </div>
               </div>
+              <?php endif; ?>
               <div class="col-12">
                 <nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
                   <div class="container">
